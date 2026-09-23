1482b964d4ce8bf6c492b0f1e1218ed29ceffc22
sheyneth2006-crypto <sheyneth2006@gmail.com>
2026-09-23 14:15:45 -0600

    Update SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -16,13 +16,14 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         console.error("No se encontró el botón btnConectar en el HTML");
     }
-});
-const btnBuscar = document.getElementById('btnBuscar');
+    const btnBuscar = document.getElementById('btnBuscar');
     if (btnBuscar) {
         btnBuscar.addEventListener('click', buscarCategoria);
     } else {
         console.error("No se encontró el botón btnBuscar en el HTML");
     }
+});
+
 // 4. Función que se ejecuta al hacer clic en CONECTAR
 function conectarSupabase() {
     try {
